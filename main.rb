03846db11399ff27db58356a250cd0c5bdd00391
omerbd21 <omerbendavid21@gmail.com>
2021-01-02 10:50:39 +0200

    changed bind & port; refactored get_nodes

--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
 require 'sinatra'
 require 'net/ssh'
-require_relative 'api/connect'
+require_relative 'api/get_nodes'
 require_relative 'api/addNode'
 require_relative 'api/createModule'
 require_relative 'api/removeNode'
 require_relative 'api/fetchFacts'
+set :bind, '0.0.0.0'
+set :port, 4000
